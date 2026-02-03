8f8a6576cc840bfdaf888aa1ec974c8c7e4efea2
Muhammad Danial <m.danial465@gmail.com>
2026-02-03 23:57:49 +0500

    Tasks service created.

---
+++ src/services/taskservice.js
@@ -0,0 +1,69 @@
+import api from './api';
+import authservice from './authservice';
+
+
+//add JWt token to all requests
+api.interceptors.request.use((config) => 
+{
+    const token = authservice.getToken();
+    if(token){
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
+const taskService = {
+    //Get all tasks for logged-in user
+    getTasks: async () => {
+        try{
+            const response = await api.get('/api/tasks');
+            return response.data;
+        }catch (error){
+            throw error.response?.data || error.message;
+        }
+    },
+
+    //Get single task by ID
+    getTaskById: async(id) => {
+        try{
+            const response = await api.get(`/api/tasks/${id}`);
+            return response.data;
+        }catch (error){
+            throw error.response?.data || error.message;
+        }
+    },
+
+    //Create new task
+    CreatedTask: async (taskData) => {
+        try{
+            const response = await api.post(`/api/tasks`, taskData);
+            return response.data;
+        }catch(error) {
+            throw error.response?.data || error.message;
+        }
+    },
+    //Update existing task
+    updateTask: async (id, taskData) =>{
+        try{
+            const response = await api.put(`/api/tasks/${id}`, taskData);
+            return response.data;
+        }catch (error){
+            throw error.response?.data || error.message;
+        }
+    },
+    // Toggel task completion status (uses update endpoint)
+    toggleTaskStatus: async (id, currentTask) => {
+        try{
+            const updatedTask = {
+                ...currentTask,
+                completed: !currentTask.completed
+            };
+            const response = await api.put(`/api/tasks/${id}`, updatedTask);
+            return response.data;
+        }catch (error){
+            throw error.response?.data || error.message;
+        }
+    }
+};
+
+export default taskService;
